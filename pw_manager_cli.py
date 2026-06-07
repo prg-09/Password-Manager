93b89767e0f4838fd1a313787b822456729a3bcc
Pragya Dhungel <pragyadhungel2006@gmail.com>
2026-06-07 17:32:56 +0545

    Formatted document

--- pw_manager_cli.py
+++ pw_manager_cli.py
@@ -1,29 +1,31 @@
-
 from getpass import getpass
 import json
-import os 
+import os
 import string
 import secrets
 from cryptography.fernet import Fernet
 
 pws = []
 
+
 def load_pws():
     if os.path.exists("pw.json"):
-        with open ("pw.json") as f:
+        with open("pw.json") as f:
             return json.load(f)
     else:
         return []
-    
+
+
 pws = load_pws()
 
+
 def dump_pws():
-    with open ("pw.json","w") as f:
-        json.dump(pws,f, indent= 4)
+    with open("pw.json", "w") as f:
+        json.dump(pws, f, indent=4)
 
 
-def pw_generator(length = 10):
-    characters = (string.ascii_letters + string.punctuation + string.digits)
+def pw_generator(length=10):
+    characters = string.ascii_letters + string.punctuation + string.digits
     password = ""
     for _ in range(length):
         password += secrets.choice(characters)
@@ -35,26 +37,26 @@ def pw_strength_checker(password):
     while True:
         special_chars = string.punctuation
         has_uppercase = any(char.isupper() for char in password)
-        has_lowercase  = any (char.islower() for char in password)
-        has_digit = any(char.isdigit() for char in password) 
+        has_lowercase = any(char.islower() for char in password)
+        has_digit = any(char.isdigit() for char in password)
         has_symbol = any(char in special_chars for char in password)
         has_min_length = len(password) >= 10
-        rules = [has_uppercase,has_lowercase,has_digit,has_symbol, has_min_length]
-        
+        rules = [has_uppercase, has_lowercase, has_digit, has_symbol, has_min_length]
+
         score = sum(rules)
-        
+
         if all(rules):
             print("Strong Password")
             break
         elif score >= 3:
-            print ("Moderate Password")
+            print("Moderate Password")
             break
         else:
             print("Weak Password")
             password = getpass("Please enter strong password:")
-            
+
     return password
-        
+
 
 def add():
     website = input("Website / App: ")
@@ -63,11 +65,13 @@ def add():
     for pw in pws:
         if website.lower() in pw["website"].lower():
             duplicate = True
-            print (f"Password for {website} already exists. What do you want to do?")
+            print(f"Password for {website} already exists. What do you want to do?")
             while True:
                 try:
-                    choice = int(input("\n1.Update the existing password.\n2.Add it anyway "))
-                    
+                    choice = int(
+                        input("\n1.Update the existing password.\n2.Add it anyway ")
+                    )
+
                     if choice == 1:
                         edit()
                         return
@@ -75,56 +79,57 @@ def add():
                         duplicate = False
                         stop_loop = True
                         break
-                        
+
                 except ValueError:
                     print("Enter valid choice ")
-                    
+
         if stop_loop:
             break
-                
+
     if not duplicate:
         username = input("Username: ")
         while True:
-            choice = input("Do you want to generate password instead of typing yourself?(y/n): ")
-            
+            choice = input(
+                "Do you want to generate password instead of typing yourself?(y/n): "
+            )
+
             if choice.lower() == "y":
                 password = pw_generator()
                 break
             elif choice.lower() == "n":
                 password = getpass("Password:")
                 password = pw_strength_checker(password)
-                
+
                 break
             else:
                 print("Please enter y or n")
         pw = {
             "website": website,
             "username": username,
-            "password": encrypt_password(password)
+            "password": encrypt_password(password),
         }
-        
+
         pws.append(pw)
         dump_pws()
 
 
-
 def view():
-   
+
     if not pws:
-        print ("No password stored to view")
+        print("No password stored to view")
         return
     else:
         for i, pw in enumerate(pws):
-            print (f'\n{i+1}. Website: {pw["website"]} | Username: {pw["username"]} ')
-    
-    
+            print(f'\n{i+1}. Website: {pw["website"]} | Username: {pw["username"]} ')
+
+
 def search():
     search_key = input("Which web/app pw do you want to search? ").lower()
 
     found = False
-    
+
     for pw in pws:
-        
+
         if search_key in pw["website"].lower():
             print(f"Website: {pw['website']}")
             print(f"Username: {pw['username']}")
@@ -134,11 +139,11 @@ def search():
             break
 
     if not found:
-        print("Password not found") 
+        print("Password not found")
 
 
 def edit():
-    
+
     if not pws:
         print("No pw stored")
         return
@@ -152,36 +157,36 @@ def edit():
                 new_website = input(f"Website(Current:{pw['website']}): ")
                 new_username = input(f"Username(Current:{pw['username']}): ")
                 new_password = getpass("Password: ")
-                
-                if new_website :
+
+                if new_website:
                     pw["website"] = new_website
-                
-                if new_username :
+
+                if new_username:
                     pw["username"] = new_username
-                    
-                if new_password :
+
+                if new_password:
                     new_password = pw_strength_checker(new_password)
                     pw["password"] = encrypt_password(new_password)
-           
+
                 dump_pws()
                 break
             else:
                 print("Enter a valid integer")
         except ValueError:
-            print("Enter an integer")    
-            
-     
+            print("Enter an integer")
+
+
 def delete():
     if not pws:
-        print ("No pw available to delete")
+        print("No pw available to delete")
         return
     while True:
         view()
         try:
-            
+
             delete_num = int(input("Enter the number of pw you want to delete: "))
-            if 1 <= delete_num <= len(pws): 
-                deleted = pws.pop(delete_num-1)
+            if 1 <= delete_num <= len(pws):
+                deleted = pws.pop(delete_num - 1)
                 print(f"Succesfully deleted pw of {deleted['website']} ")
                 dump_pws()
                 break
@@ -217,10 +222,13 @@ def decrypt_password(encrypted_password):
     return decrypted_password
 
 
-    
 while True:
     try:
-        menu_chosen = int(input("\n------Menu------\n1.Add Password \n2.View \n3.Search \n4.Edit \n5.Delete \n6.Exit\n"))
+        menu_chosen = int(
+            input(
+                "\n------Menu------\n1.Add Password \n2.View \n3.Search \n4.Edit \n5.Delete \n6.Exit\n"
+            )
+        )
         if menu_chosen == 1:
             add()
         elif menu_chosen == 2:
@@ -236,6 +244,4 @@ while True:
         else:
             print("Please enter a valid number from 1 to 6")
     except ValueError:
-        print ("Please enter a number from 1 to 6")
-    
-
+        print("Please enter a number from 1 to 6")
